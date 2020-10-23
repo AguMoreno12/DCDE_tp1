1e3f8f980cf95e8e16c2348eca59f4f1e17cf6d7
Agustin <ingmorenoagustin@gmail.com>
2020-10-22 23:03:28 -0300

    nuevo contenedor con linux para el comando bash

--- BASH/comando2.bash
+++ BASH/comando2.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 PASS="admin"
 USER="root"
-mysql -u$USER -p$PASS <<MYSQL_SCRIPT
+mysql -u$USER -p$PASS -h basedatos<<MYSQL_SCRIPT
 CREATE SCHEMA bdPruebaTp;
 USE bdPruebaTp;
 CREATE TABLE beacons (  DistanceA DOUBLE NULL,  DistanceB DOUBLE NULL,  DistanceC DOUBLE NULL,  PositionX INT NULL,  PositionY INT NULL,  DateTime DATETIME NOT NULL,  PRIMARY KEY (DateTime));
